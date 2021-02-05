f53a6df557a7bc593f86daedad3ec8a764323e48
akroyce25 <akr0030@mix.wvu.edu>
2021-02-05 17:40:20 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p class="c1">alsdjkflajdflasjdfljdljflsjflalshfldjhflkj</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Soccer Cleats</p>
+                    <p>Outdoor and Indoor Cleats</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Soccer Balls</p>
+                    <p>Sizes 1-5</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Soccer Uniforms</p>
+                    <p>Jerseys, Shorts, Shinguards, and Socks</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Soccer Bags</p>
+                    <p>Regular, Single Strap, and Drawstring Bags</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
